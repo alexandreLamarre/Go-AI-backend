a8acd262880777881073916e8d1fd21d44197da2
alexandreLamarre <alex7285@gmail.com>
2020-09-20 19:48:58 -0400

    small routing changes

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
 GAME_CACHE = {}
 
 
-from flask import Flask
+from flask import Flask, request, jsonify
 from src.Go import Go
 app = Flask(__name__)
 
-@app.route("/createNewBoard")
-def createBoard(uuid, board):
-     go_board = Go(board)
-     GAME_CACHE[uuid] = go_board
+@app.route("/createNewBoard", methods=["POST"])
+def createBoard():
+    content = request.json()
+    # go_board = Go(board)
+    # GAME_CACHE[uuid] = go_board
 
 @app.route("/playMovePlayer")
 def playNextMove(uuid, player):
     go_board = GAME_CACHE[uuid]
 
-    return go_board.get_board, "meaningful message"
+
 
 @app.route("/playMoveAI")
 def playNextMoveAI(uuid, player, AItype):
     go_board = GAME_CACHE[uuid]
-    return go_board.get_board, "meaningful message "
 
 if __name__ == "__main__":
     app.run()
