7744eaf3d44ca2a6fce0f49510bc3acc48268d62
alexandreLamarre <alex7285@gmail.com>
2020-09-20 19:21:02 -0400

    Commiting initial go board logic

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
+GAME_CACHE = {}
+
+
 from flask import Flask
+from src.Go import Go
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/createNewBoard")
+def createBoard(uuid, board):
+     go_board = Go(board)
+     GAME_CACHE[uuid] = go_board
+
+@app.route("/playMovePlayer")
+def playNextMove(uuid, player):
+    go_board = GAME_CACHE[uuid]
+
+    return go_board.get_board, "meaningful message"
+
+@app.route("/playMoveAI")
+def playNextMoveAI(uuid, player, AItype):
+    go_board = GAME_CACHE[uuid]
+    return go_board.get_board, "meaningful message "
 
 if __name__ == "__main__":
     app.run()
---
+++ src/Go.py
@@ -0,0 +1,111 @@
+class Go:
+    def __init__(self, board):
+        self.board = board
+        self.neighbors = []
+        for i in range(len(board)):
+            neighbor_row = []
+            for j in range(len(board)):
+                neighbor_row.append(get_valid_neighbors(i,j))
+            self.neighbors.append(neighbor_row)
+        self.previousMove = None
+        self.gameOver = false
+
+    def get_board(self):
+        return self.board
+
+    def set_move(self, player, move, x, y):
+        if(move == "pass" and not(self.previousMove)):
+            self.previousMove = true
+        elif(move == "pass" and self.previousMove):
+            self.gameOver
+        else:
+            play(player,x,y)
+
+    def play(self, player, x, y):
+        self.board[y][x] = player
+        self.updateBoard(x,y)
+
+    def place_stone(self,player,x,y):
+        new_board = list(list(board_row for board_row in self.board))
+        new_board[y][x] = player
+        return new_board
+
+    def is_on_board(self,x,y):
+        return (x % len(board) == x) and (y % len(board) == y)
+
+    def get_valid_neighbors(self,x,y):
+        possible_neighbors = ((x+1,y), (x-1,y), (x,y+1), (x, y-1))
+        return [self.board[p[1]][p[0]] for p in possible_neighbors if is_on_board(p)]
+
+    def find_reached(self,board,x,y):
+        color = board[y][x]
+        chain = set((x,y))
+        reached = set()
+        frontier = [(x,y)]
+        while(frontier):
+            current_pos = frontier.pop()
+            chain.add(current_pos)
+            for node in self.neighbors[current_pos[1]][current_pos[0]]:
+                if board[node[1]][node[0]] == color and not node in chain:
+                    frontier.append(node)
+                elif board[fn] != color:
+                    reached.add(fn)
+
+        return chain, reached
+
+    def check_captures(self,board, x,y):
+        chain, reached = find_reached(x,y)
+        non_empty = 0
+        for i in range(len(reached)):
+            if(board[reached[1]][reached[0]]):
+                non_empty += 1
+        if non_empty == len(reached):
+            for i in range(len(chain)):
+                board[chain[1]][chain[0]] = 0
+            return board, chain
+        else:
+            return board, {}
+
+    def swap_players(self,player):
+        if player == 1:
+            return 2
+        if player == 2:
+            return 1
+
+    def play_move(self,player,x,y):
+        ouput = ""
+        if(self.board[y][x]):
+            output += "Illegal move"
+            return self.board, output
+
+        board = place_stone(player,x,y)
+        oppponent = swap_players(player)
+
+        player_stones = []
+        opponent_stones = []
+
+        for node in self.neighbors[y][x]:
+            if(board[y][x] == player):
+                my_stones.append((x,y))
+            if(board[y][x] == opponent):
+                opponent_stones.append((x,y))
+        chain = {}
+        for s in opponent_stones:
+            board, chain = check_captures(board, s[0],s[1])
+            output += str(chain)
+        if output:
+            color = ""
+            if(player == 1): color = "White"
+            if(player == 2): color = "Black"
+            output = color + " captured :" + output
+        output2 = ""
+        for s in player_stones:
+            board,chain = check_captures(board, s[0],s[1])
+            output2 += str(chain)
+        if output2:
+            if(player == 1): color = "White"
+            if(player == 2): color = "Black"
+            output2 = color + " captured : " + output2
+
+        self.board = board
+        return board, output+output2
